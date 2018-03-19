676132e9570bea13cc487f207ea84bff606194c5
kocjanr <kocjanr@onid.oregonstate.edu>
2018-03-19 14:07:25 -0700

    updates

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -20,28 +20,81 @@ public class UrlValidatorTest extends TestCase {
    
    
    public void testManualTest()
-   {
-//You can use this function to implement your manual testing	   
+   {   
+	   System.out.print("testManualTest()");
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   boolean test1 = urlVal.isValid("test://www.google.com");
+	   boolean test2 = urlVal.isValid("http://www.google.com:32");
+	   boolean test5 = urlVal.isValid("http:/www.google.com:32/test?arg=a");
+	   boolean test7 = urlVal.isValid("http://255.255.255.255:32/test?arg=view");
+	   boolean test8 = urlVal.isValid("://www.google.com:32/test?arg=view");
+	   boolean test9 = urlVal.isValid(" www.google.com:32/test?arg=view");
+	   boolean test10 = urlVal.isValid("test://www.google.com/test?action=view");
+	   boolean test11 = urlVal.isValid("www.google.com");
+	   boolean test12 = urlVal.isValid("http:www.google.com:32/test?arg=a");
+	   boolean test13 = urlVal.isValid(" ");
 	   
+	   System.out.printf("\n");
+	   System.out.print(test1);
+	   System.out.printf("\n");
+	   System.out.print(test2);
+	   System.out.printf("\n");
+	   System.out.print(test5);
+	   System.out.printf("\n");
+	   System.out.print(test7);
+	   System.out.printf("\n");
+	   System.out.print(test8);
+	   System.out.printf("\n");
+	   System.out.print(test9);
+	   System.out.printf("\n");
+	   System.out.print(test10);
+	   System.out.printf("\n");
+	   System.out.print(test11);
+	   System.out.printf("\n");
+	   System.out.print(test12);
+	   System.out.printf("\n");
+	   System.out.print(test13);
+	   System.out.printf("\n");
+
    }
-   
    
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
+	 UrlValidator url = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	 String[] correctURLS = {"http://www.google.com","http://www.google.com/stuff", "ftp://www.google.com",  "ftp://www.google.net",  "http://www.google.net"};
+	 for(int i=0; i < correctURLS.length;i++){
+		 assert(url.isValid(correctURLS[i]) == true);
+	 }	
 
    }
    
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-
+		 UrlValidator url = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+		 String[] incorrectURLS = {"test://www.google.com:80","www.google.com/", " "};
+		 for(int i=0; i < incorrectURLS.length;i++){
+			 assert(url.isValid(incorrectURLS[i]) == false);
+		 }	
    }
-   //You need to create more test cases for your Partitions if you need to 
-   
-   public void testIsValid()
-   {
-	   //You can use this function for programming based testing
 
+   
+   public void testIsValidPrograming()
+   {
+	   UrlValidator url = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   String[] prefix = {"http", "http://", "test","test://"," "};
+	   String[] mainURL = {"www.google.com", "www.google.com:32", "www.google.com/test", " "};
+	   
+	   
+	   for(int i=0;i < prefix.length; i++){
+		   for(int j= 0; j< mainURL.length; j++){
+
+			   boolean assertion = url.isValid(prefix[i] + mainURL[j]);
+			   if(assertion == true){
+				   System.out.printf(prefix[i] + mainURL[j] + "\n");
+			   }
+		   } 
+	   }
+	   
    }
    
 
